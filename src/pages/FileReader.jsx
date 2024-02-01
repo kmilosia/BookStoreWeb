5373e346b43c81367f8653317f68d22a09d98832
klaudiamilosia <klaudiamiloszewska100@gmail.com>
2024-02-01 18:46:26 +0100

    commit

--- src/pages/FileReader.jsx
+++ src/pages/FileReader.jsx
@@ -5,6 +5,8 @@ import axiosClient from '../utils/api/axiosClient'
 import PageLoader from '../components/elements/PageLoader'
 import { Document, Page, pdfjs } from 'react-pdf'
 import { IoMdArrowRoundBack, IoMdArrowRoundForward } from "react-icons/io";
+import 'react-pdf/dist/Page/TextLayer.css'
+import 'react-pdf/dist/Page/AnnotationLayer.css'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 function FileReader() {
@@ -15,6 +17,8 @@ function FileReader() {
     const [numPages, setNumPages] = useState(null)
     const [pageNumber, setPageNumber] = useState(1)
     const [pdfBlob, setPdfBlob] = useState(null)
+    const [pageWidth, setPageWidth] = useState(0)
+    const pageWrapperRef = useRef(null) 
     const getBookFile = async (id) => {
         try {
             const token = getValidToken()
@@ -37,11 +41,6 @@ function FileReader() {
     const onDocumentLoadSuccess = ({ numPages }) => {
         setNumPages(numPages)
     }
-    useEffect(() => {
-        if(bookID){
-            getBookFile(bookID)
-        }
-    },[])
     const handleKeyDown = (event) => {
         if (event.key === 'ArrowLeft' && pageNumber > 1) {
             setPageNumber(pageNumber - 1)
@@ -51,19 +50,37 @@ function FileReader() {
             setPageNumber(pageNumber + 1)
         }
     }
+    useEffect(() => {
+        if(bookID){
+            getBookFile(bookID)
+        }
+    },[])
     window.addEventListener('keydown', handleKeyDown)
+    useEffect(() => {
+        const updatePageWidth = () => {
+            const currentRef = pageWrapperRef.current
+            if (currentRef) {
+                const width = currentRef.getBoundingClientRect().width
+                setPageWidth(width)
+            }
+        }
+        window.addEventListener('resize', updatePageWidth)
+        updatePageWidth()
+        return () => {
+            window.removeEventListener('resize', updatePageWidth)
+        }
+    }, [pageWrapperRef.current])
+    useEffect(() => {
+        console.log(pageWidth);
+    },[pageWidth])
   return (
     loading ? <PageLoader /> :
     <div className='default-page-wrapper'>
     <div className='default-page-container'>
     {access ?
-     <>
-     <Document
-         file={pdfBlob}
-         onLoadSuccess={onDocumentLoadSuccess}
-         onLoadError={(error) => console.log('Błąd podczas pobierania PDF:', error)}
-     >
-         <Page pageNumber={pageNumber} width={1200}/>
+     <div ref={pageWrapperRef}>
+     <Document file={pdfBlob} onLoadSuccess={onDocumentLoadSuccess} onLoadError={(error) => console.log('Błąd podczas pobierania PDF:', error)}>
+        <Page pageNumber={pageNumber} width={pageWidth}/>
      </Document>
      <div className='flex flex-row items-center justify-center mt-2'>
         {pageNumber !== 1 &&
@@ -72,7 +89,7 @@ function FileReader() {
         {pageNumber < numPages &&
         <button onClick={() => {setPageNumber(pageNumber + 1);window.scrollTo(0,0)}}><IoMdArrowRoundForward size={20} className='text-purple-400'/></button>}
      </div>
-    </>
+    </div>
     :
     <div className='items-center justify-center flex flex-col min-h-[80vh]'>
         <img src='https://iili.io/Jl2XNBn.png' width={200} height={200} alt='Access key icon'/>
